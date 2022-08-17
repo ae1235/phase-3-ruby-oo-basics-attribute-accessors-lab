e3f99f855092aa04d57d0e4c150ca3827ebfdffe
+ <+>
2022-08-17 19:33:39 +0300

    Done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,22 @@
 ## code your solution here. 
+require 'pry'
+class Cat
+    attr_accessor :name
+
+    def name
+        @name
+    end
+
+    def name=(cat_name)
+        @name = cat_name
+    end
+
+    def meow
+        puts "meow!"
+    end
+    
+end
+
+cat_nm = Cat.new
+cat_nm.name = 'Fuzzy'
+binding.pry
